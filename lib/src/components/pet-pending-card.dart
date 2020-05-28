26435360e202f2be3b258726d96bdcba7120432c
Javier Gómez <f.gomez@astoundcommerce.com>
2020-05-28 16:09:16 -0500

    add wasap lauch

--- lib/src/components/pet-pending-card.dart
+++ lib/src/components/pet-pending-card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:getflutter/getflutter.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:flutter_open_whatsapp/flutter_open_whatsapp.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:pet_son_app/src/modules/pet/model.dart';
 
 Widget drawPetPending(Pet pet) {
@@ -25,11 +25,17 @@ Widget drawPetPending(Pet pet) {
  );
 }
 
-_openWSP(Pet pet) {
+_openWSP(Pet pet) async {
+  String whatsappUrl;
   try {
+    String phoneNumber = '+573212975798';
     String _message = 'Hola! quisiera saber sobre la mascota ${pet.name}.';
-    FlutterOpenWhatsapp.sendSingleMessage('3212975798', _message);
+    whatsappUrl = "whatsapp://send?phone=$phoneNumber&text=$_message";
+    if (await canLaunch(whatsappUrl)) {
+      await launch(whatsappUrl);
+    }
   } catch (ex) {
+    print('Could not launch $whatsappUrl');
     print(ex.toString());
   }
 }
